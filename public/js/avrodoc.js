465cd7b6b343d74d2f3708e614fcdfc9b5f24791
Martin Kleppmann <martin@rapportive.com>
2012-10-14 22:43:57 -0700

    Add popovers for named types

--- public/js/avrodoc.js
+++ public/js/avrodoc.js
@@ -4,14 +4,63 @@ function AvroDoc(input_schemata) {
     var list_pane = $('#list-pane'), content_pane = $('#content-pane');
     var schema_by_name = {};
 
+    // popover_by_name[filename][qualified_name] = {title: 'html', content: 'html'}
+    var popover_by_name = {};
+
+    // Render all the popovers ahead of time, because Dust's rendering is async but the popover
+    // plugin expects to receive the popover content synchronously when it is triggered.
+    function renderPopovers() {
+        _(schema_by_name).each(function (schema, filename) {
+            popover_by_name[filename] = {};
+            _(schema.named_types).each(function (type, qualified_name) {
+                var popover = popover_by_name[filename][qualified_name] = {};
+
+                // Do the actual rendering in the background, to keep the page responsive
+                window.setTimeout(function () {
+                    dust.render('popover_title', type, function (err, html) {
+                        popover.title = html;
+                    });
+                    dust.render('named_type_details', type, function (err, html) {
+                        popover.content = html;
+                    });
+                }, 100);
+            });
+        });
+    }
+
+    // Configures all links to types in the current content pane to show popovers on hover.
+    function setupPopovers() {
+        content_pane.find('a[href^="#/schema/"]').each(function () {
+            var url_segments = $(this).attr('href').split('/');
+            var schema_popovers = popover_by_name[decodeURIComponent(url_segments[2])];
+            if (!schema_popovers) return;
+            var popover = schema_popovers[decodeURIComponent(url_segments[3])];
+            if (!popover) return;
+
+            $(this).popover({
+                trigger: 'hover',
+                placement: 'bottom',
+                title: function () { return popover.title; },
+                content: function () { return popover.content; },
+                delay: {show: 200, hide: 50},
+                template: '<div class="popover avrodoc-named-type"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><div></div></div></div></div>'
+            });
+        });
+    }
+
+    // Renders the details of the given type in the main content pane.
     function showType(type) {
+        // Clean up old content
         list_pane.find('li').removeClass('selected');
+        $('body > .popover').remove();
+        $('body').scrollTop(0);
 
         if (!type) {
             content_pane.empty();
         } else {
             dust.render('named_type', type, function (err, html) {
                 content_pane.html(html);
+                setupPopovers();
             });
 
             // Mark the currently displayed type with a 'selected' CSS class in the type list
@@ -21,10 +70,12 @@ function AvroDoc(input_schemata) {
         }
     }
 
+    // Call this once when the schemata have been loaded and we want to launch the app.
     function ready() {
         // Fields used by the schema_list template
         _public.schemata = _(schema_by_name).values();
         _public.has_multiple_schemata = (_public.schemata.length > 1);
+        renderPopovers();
 
         dust.render('schema_list', _public, function (err, html) {
             list_pane.html(html);
