97c823239c31a4951647db8200347c6bd9eedb8b
Martin Kleppmann <martin@rapportive.com>
2012-10-19 20:10:34 -0700

    Web app serves all schemata found in the 'schemata' directory

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var glob = require('glob');
+var fs = require('fs');
 var content = require('./lib/static_content');
 
-var schemata = [{filename: 'example.avsc'}];
+var schemata = [];
+glob('schemata/**/*.avsc', function (err, files) {
+    if (err) throw err;
+    files.sort().forEach(function (file) {
+        schemata.push({filename: file});
+    });
+});
 
 // Precompile dust templates at app startup, and then serve them out of memory
 var dust_templates = content.dustTemplates();
@@ -27,6 +35,13 @@ app.get('/', function (req, res, next) {
 
 app.get('/dust-templates.js', function (req, res, next) {
     res.set('Content-Type', 'text/javascript').send(dust_templates);
+});
+
+app.get(/^\/(schemata(?:\/\w[\w.\-]*)+)$/, function (req, res) {
+    fs.readFile(path.join(__dirname, req.params[0]), 'utf-8', function (err, data) {
+        if (err) throw err;
+        res.set('Content-Type', 'application/json').send(data);
+    });
 });
 
 http.createServer(app).listen(app.get('port'), function () {
