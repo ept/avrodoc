13abdb2a333179d07598d683912ab439a33c9294
Micah Huff <micah@micahhuff.com>
2013-05-01 16:12:54 -0700

    Revert "Do not check the shared_schema for equality comparison but rather the root schema. Resolves #14"
    This reverts commit 851cf994d3138a0544fc9dbcbf029787885f6e42.

--- public/js/schema.js
+++ public/js/schema.js
@@ -252,7 +252,7 @@ AvroDoc.Schema = function (avrodoc, shared_types, schema_json, filename) {
 
         if (_(shared_types).has(qualified_name)) {
             shared_schema = _(shared_types[qualified_name]).find(function (shared_schema) {
-                return _(new_type).isEqual(typeEssence(schema));
+                return _(new_type).isEqual(typeEssence(shared_schema));
             });
         } else {
             shared_types[qualified_name] = [];
